d7f376cae5dffe39ef7f94d91b8e139130d5b678
Yassine Malki <ymalki33@gmail.com>
2026-04-23 17:10:34 +0100

    Remove LanguageProvider from main.tsx
    - Removed LanguageContext import
    - Removed LanguageProvider wrapper component
    - Application now renders without language context

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
-import { LanguageProvider } from './contexts/LanguageContext'
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <LanguageProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </LanguageProvider>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>
 );
